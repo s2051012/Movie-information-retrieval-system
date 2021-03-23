3d68e3c4aa31ea27fc3ceae136297a22961907aa
xr980704 <44341368+xr980704@users.noreply.github.com>
2021-03-23 21:59:45 +0800

    Add files via upload

--- ViewModel/scrapy/evaluate_scrapy.py
+++ ViewModel/scrapy/evaluate_scrapy.py
@@ -3,7 +3,9 @@ import requests
 import time
 import json
 from bs4 import BeautifulSoup
-from ViewModel.utils import preprocessing
+import sys
+sys.path.append("..") 
+from utils import preprocessing
 
 search_url = 'https://www.imdb.com/find?q={}&s=tt&ref_=fn_al_tt_mr'
 
@@ -53,13 +55,65 @@ def search_batch(query_list):
 
 
 if __name__ == '__main__':
-    query_list = [
-        'star war',
-        'harry potter',
+    film_query_list = [
+        'Black Panther',
+        'harry pottter and',
+        'X-Men',
+        'The Avengers',
+        'Avengers: Endgame',
+        'Resident Evil and',
+        'Black',
+        'The Wizarrd of Oz',
+        'A Star Is Born',
+        'A Star Born',
+        'A Quiet Place',
+        'The Quiet Place',
+        'A Night at the Opera',
+        'A Night',
+        'It Happened One Night',
+        'It Happened Two Night',
+        'Star Wars: The Last Jedi',
+        'Star',
+        'The Adventures of Robin Hood',
+        'Leave No Trace',
+        'Leave Any Trace',
+        'Spider-Man: Far From Home',
+        'Spider',
+        'The Invisible Man',
+        'La La Land',
+        'La',
+        'Hell or High Water',
+        'The Dark Knight',
+        'The Godfather',
+        'The Knight',
+        'Top Hat',
+        'The Maltese Falcon',
+        'The Mandese Falcon',
+        'Sunset Boulevard',
+        'Sunset',
+        'Shadow of a Doubt',
+        'Shadow',
+        'Call Me by Your Name',
+        '12 Angry Men',
+        '12',
+        'Schindler',
+        'Fight Club',
+        'The Usual Suspects',
+        'The Unusual Suspects',
+        'The Lion King',
+        'The Tiger King',
+        'Back to the Future',
+        'Shining',
+        'Good Will Hunting',
+        'Good Willing Hunting'
     ]
-    dict_of_list = search_batch(query_list)
-    for k, v in dict_of_list.items():
-        print(k, v)
-    file = open(os.path.join(os.path.curdir, 'eval.json'), 'w')
-    json.dump(dict_of_list, file, indent=1)
-    file.close()
+
+    
+    film_dict_of_list = search_batch(film_query_list)
+    #for k, v in film_dict_of_list.items():
+        #print(k, v)
+    #for k, v in actor_dict_of_list.items():
+        #print(k, v)
+    film_file = open(os.path.join(os.path.curdir, 'film_eval.json'), 'w')
+    json.dump(film_dict_of_list, film_file, indent=1)
+    film_file.close()
